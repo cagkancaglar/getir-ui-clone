872bd60e86bc8a4707ffddba2b0279ebad0ee056
Çağkan Çağlar <cagkancaglar@hotmail.com>
2022-12-30 18:33:05 +0300

    mobile-app

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
   theme: {
     extend: {
+      backgroundImage: {
+        'mobile-app': "url('https://cdn.getir.com/getirweb-images/common/illustration/doodle.png')"
+      },
       spacing: {
         '0.1': '0.063rem',
       },
